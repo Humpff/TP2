a882378b5f7af6fa50240d9fb458ea305fc68504
darnay_l <darnay_l@epitech.eu>
2013-11-07 11:22:21 +0100

    tests done

--- couvertureTesteur.cpp
+++ couvertureTesteur.cpp
@@ -43,12 +43,56 @@ TEST_F(CouvertureTest, villesCritiques)
   EXPECT_FALSE(couv.villesCritiques().empty());
 }
 
-TEST_F(CouvertureTest, determinerMinParcours)
+TEST_F(CouvertureTest, dureeMinParcours)
 {
   tp2::Ville depart("ville1", "a1");
   tp2::Ville arrivee("ville4", "d4");
   int duree;
   couv.determinerMinParcours(depart, arrivee, duree);
+  EXPECT_EQ(duree, 90);
+}
+
+TEST_F(CouvertureTest, dureeMinParcoursImpossible)
+{
+  tp2::Ville depart("ville1", "a1");
+  tp2::Ville arrivee("ville4", "d4");
+  int duree;
+  fcouv.determinerMinParcours(depart, arrivee, duree);
+  EXPECT_EQ(duree, -1);
+}
+
+TEST_F(CouvertureTest, dureeMinParcoursException)
+{
+  tp2::Ville depart("ville1", "a1");
+  tp2::Ville arrivee("ville7", "g7");
+  int duree;
+  EXPECT_THROW(couv.determinerMinParcours(depart, arrivee, duree), std::logic_error);
+}
+
+TEST_F(CouvertureTest, bellmanFord)
+{
+  tp2::Ville depart("ville1", "a1");
+  tp2::Ville arrivee("ville4", "d4");
+  int duree;
+  couv.bellManFord(depart, arrivee, duree);
+  EXPECT_EQ(duree, 90);
+}
+
+TEST_F(CouvertureTest, bellmanFordImpossible)
+{
+  tp2::Ville depart("ville1", "a1");
+  tp2::Ville arrivee("ville4", "d4");
+  int duree;
+  fcouv.bellManFord(depart, arrivee, duree);
+  EXPECT_EQ(duree, -1);
+}
+
+TEST_F(CouvertureTest, bellmanFordException)
+{
+  tp2::Ville depart("ville1", "a1");
+  tp2::Ville arrivee("ville7", "g7");
+  int duree;
+  EXPECT_THROW(couv.bellManFord(depart, arrivee, duree), std::logic_error);
 }
 
 int main(int ac, char* av[])
