0ea34ceb65089fc559e22efd3db03d618c057c31
darnay_l <darnay_l@epitech.eu>
2013-11-07 08:17:07 +0100

    c'est trop nul

--- Couverture.cpp
+++ Couverture.cpp
@@ -12,175 +12,175 @@
 
 namespace tp2
 {
-//! \brief		Constructeur à partir d'un fichier en entrée.
-//! \param[in]	p_fichierEntree pointeur sur le fichier contenant la liste des villes à charger ainsi que les liaisons.
-//! \pre		Il y a assez de mémoire pour charger toutes les ville de la liste.
-//! \pre		Le fichier \a fichierEntree est ouvert corectement.
-//! \post		Le fichier \a fichierEntree n'est pas fermé par la fonction.
-//! \post		Si les préconditions sont respectées, les données de la couverture contenues
-//!				dans le fichier en entrée sont organisées dans un graphe en mémoire.
-//! \exception	bad_alloc si pas assez de mémoire contenir toute la liste du fichier.
-//! \exception	logic_error si le fichier \a fichierEntree n'est pas ouvert correctement.
-Couverture::Couverture(std::ifstream & p_fichierEntree)
-{
-	if (p_fichierEntree == NULL)
-		throw std::logic_error("Couverture: fichier non ouvert\n");
+  //! \brief		Constructeur à partir d'un fichier en entrée.
+  //! \param[in]	p_fichierEntree pointeur sur le fichier contenant la liste des villes à charger ainsi que les liaisons.
+  //! \pre		Il y a assez de mémoire pour charger toutes les ville de la liste.
+  //! \pre		Le fichier \a fichierEntree est ouvert corectement.
+  //! \post		Le fichier \a fichierEntree n'est pas fermé par la fonction.
+  //! \post		Si les préconditions sont respectées, les données de la couverture contenues
+  //!				dans le fichier en entrée sont organisées dans un graphe en mémoire.
+  //! \exception	bad_alloc si pas assez de mémoire contenir toute la liste du fichier.
+  //! \exception	logic_error si le fichier \a fichierEntree n'est pas ouvert correctement.
+  Couverture::Couverture(std::ifstream & p_fichierEntree)
+  {
+    if (p_fichierEntree == NULL)
+      throw std::logic_error("Couverture: fichier non ouvert\n");
 
-	//Lecture des données sur les villes
-	string nom; //Pour le nom des villes
-	string codeAeroport;
-	int numero = 1;
-	vector<string> tabNomVilles;
+    //Lecture des données sur les villes
+    std::string nom; //Pour le nom des villes
+    std::string codeAeroport;
+    int numero = 1;
+    std::vector<std::string> tabNomVilles;
 
-	bool sentinelle = false;
+    bool sentinelle = false;
 
-	while (!p_fichierEntree.eof() && sentinelle == false)
-	{
-		getline(p_fichierEntree, nom);
-		if (nom == "$")//limite de la première partie du fichier
-		{
-			sentinelle = true;
-		}
-		else
-		{
-			getline(p_fichierEntree, codeAeroport);
-			m_graphe.ajouterSommet(numero, Ville(nom, codeAeroport));
-			++numero;
-			tabNomVilles.push_back(nom);
-		}
-	}
-
-	int dureeVol;
-	string villeDestination;//nom de la ville de destination
-	int indiceSource;
-	int indiceDestination;
-	char buff[255];
-	vector<string>::iterator position;
-	while (!p_fichierEntree.eof())
-	{
-		p_fichierEntree.getline(buff, 100);
-		nom = buff;
-		position = find(tabNomVilles.begin(), tabNomVilles.end(), nom);
-		indiceSource = position - tabNomVilles.begin();
-		p_fichierEntree.getline(buff, 100);
-		villeDestination = buff;
-		position = find(tabNomVilles.begin(), tabNomVilles.end(),
-				villeDestination);
-		indiceDestination = position - tabNomVilles.begin();
-		p_fichierEntree >> dureeVol;
-		p_fichierEntree.ignore();
-		m_graphe.ajouterArc(indiceSource + 1, indiceDestination + 1, dureeVol);
-	}
-	//	cout << m_graphe;
-}
-
-//! \brief		Affiche une liste de villes de la couverture à l'écran.
-//! \param[in]	p_vVilles		Une liste de villes dans un vector.
-//! \post		Le contenu de la liste p_vVilles est affiché.
-void Couverture::afficherVilles(std::vector<Ville> & p_vVilles)
-{
-	if (p_vVilles.size() == 0)
-	{
-		std::cout << "La liste est vide\n";
-	}
+    while (!p_fichierEntree.eof() && sentinelle == false)
+      {
+	getline(p_fichierEntree, nom);
+	if (nom == "$")//limite de la première partie du fichier
+	  {
+	    sentinelle = true;
+	  }
 	else
-	{
-		for (unsigned int i = p_vVilles.size(); i > 0; i--)
-		{
-			std::cout << p_vVilles[i - 1] << std::endl;
-		}
-	}
-}
+	  {
+	    getline(p_fichierEntree, codeAeroport);
+	    m_graphe.ajouterSommet(numero, Ville(nom, codeAeroport));
+	    ++numero;
+	    tabNomVilles.push_back(nom);
+	  }
+      }
 
-//! \brief		Détermine si toutes les villes de la couverture sont accessibles à partir de n'importe où.
-//! \pre		La couverture est non vide.
-//! \exception	logic_error si le graphe ne contient aucun sommet 
-//! \return		VRAI si toute ville est accessible depuis une autre, sinon FAUX dans tous les autres cas.
-bool Couverture::villesAccessibles()
-{
-	if (m_graphe == NULL || m_graphe.estVide())
-		throw std::logic_error("La couverture est vide.");
-	return m_graphe.estFortementConnexe();
-}
+    int dureeVol;
+    std::string villeDestination;//nom de la ville de destination
+    int indiceSource;
+    int indiceDestination;
+    char buff[255];
+    std::vector<std::string>::iterator position;
+    while (!p_fichierEntree.eof())
+      {
+	p_fichierEntree.getline(buff, 100);
+	nom = buff;
+	position = find(tabNomVilles.begin(), tabNomVilles.end(), nom);
+	indiceSource = position - tabNomVilles.begin();
+	p_fichierEntree.getline(buff, 100);
+	villeDestination = buff;
+	position = find(tabNomVilles.begin(), tabNomVilles.end(),
+			villeDestination);
+	indiceDestination = position - tabNomVilles.begin();
+	p_fichierEntree >> dureeVol;
+	p_fichierEntree.ignore();
+	m_graphe.ajouterArc(indiceSource + 1, indiceDestination + 1, dureeVol);
+      }
+    //	cout << m_graphe;
+  }
 
-//! \brief		Trouve les points d'articulation de la couverture et les retourne.
-//! \pre		Il y a assez de mémoire pour retourner les points d'articulation.
-//! \post		Le graphe original reste inchangé.
-//! \return		La liste de tous les sommets d'articulation est retournée.
-//! \exception	bad_alloc s'il n'y a pas assez de mémoire pour retourner les noms des sommets.
-//! \exception	logic_error si la couverture ne contient aucune ville.
-std::vector<Ville> Couverture::villesCritiques()
-{
-	if (m_graphe == NULL || m_graphe.estVide())
-		throw std::logic_error("La couverture est vide.");
-	try
-	{
-		std::vector<Ville> villesCritiques;
-		m_graphe.getPointsArticulations(villesCritiques);
-	}
-	catch(...)
-	{
-		throw;
-	}
+  //! \brief		Affiche une liste de villes de la couverture à l'écran.
+  //! \param[in]	p_vVilles		Une liste de villes dans un vector.
+  //! \post		Le contenu de la liste p_vVilles est affiché.
+  void Couverture::afficherVilles(std::vector<Ville> & p_vVilles)
+  {
+    if (p_vVilles.size() == 0)
+      {
+	std::cout << "La liste est vide\n";
+      }
+    else
+      {
+	for (unsigned int i = p_vVilles.size(); i > 0; i--)
+	  {
+	    std::cout << p_vVilles[i - 1] << std::endl;
+	  }
+      }
+  }
+
+  //! \brief		Détermine si toutes les villes de la couverture sont accessibles à partir de n'importe où.
+  //! \pre		La couverture est non vide.
+  //! \exception	logic_error si le graphe ne contient aucun sommet 
+  //! \return		VRAI si toute ville est accessible depuis une autre, sinon FAUX dans tous les autres cas.
+  bool Couverture::villesAccessibles()
+  {
+    if (m_graphe.estVide())
+      throw std::logic_error("La couverture est vide.");
+    return m_graphe.estFortementConnexe();
+  }
+
+  //! \brief		Trouve les points d'articulation de la couverture et les retourne.
+  //! \pre		Il y a assez de mémoire pour retourner les points d'articulation.
+  //! \post		Le graphe original reste inchangé.
+  //! \return		La liste de tous les sommets d'articulation est retournée.
+  //! \exception	bad_alloc s'il n'y a pas assez de mémoire pour retourner les noms des sommets.
+  //! \exception	logic_error si la couverture ne contient aucune ville.
+  std::vector<Ville> Couverture::villesCritiques()
+  {
+    if (m_graphe.estVide())
+      throw std::logic_error("La couverture est vide.");
+    try
+      {
+	std::vector<Ville> villesCritiques;
+	m_graphe.getPointsArticulation(villesCritiques);
 
 	return villesCritiques;
-}
+      }
+    catch(...)
+      {
+	throw;
+      }
+  }
 
-//! \brief		Trouve le plus court chemin entre deux points en utilisant l'algorithme de Dijsktra et le retourne.
-//! \param[in]	p_origine la ville de l'origine.
-//! \param[in]	p_destination la ville de destination.
-//! \param[out]	p_duree le temps de vol total.
-//! \return		Un vecteur contenant la liste des villes a parcourir.
-//! \pre		Il y a assez de mémoire pour retourner les composantes du chemin.
-//! \post		Le temps de vol total est retourné, -1 s'il n'y a pas de chemin.
-//! \post		Le graphe original reste inchangé.
-//! \exception	bad_alloc s'il n'y a pas assez de mémoire pour retourner le chemin.
-//! \exception	logic_error si le sommet d'origine ou de destination n'existe pas.
-std::vector<Ville> Couverture::determinerMinParcours(const Ville & p_origine, const Ville & p_destination, int & p_duree)
-{
-	if (m_graphe == NULL || m_graphe.estVide())
-		throw std::logic_error("La couverture est vide.");
-	try
-	{
-		std::vector<Ville> chemin;
-		p_duree = m_graphe.dijkstra(p_origine, p_destination, chemin);
-	}
-	catch(...)
-	{
-		throw;
-	}
+  //! \brief		Trouve le plus court chemin entre deux points en utilisant l'algorithme de Dijsktra et le retourne.
+  //! \param[in]	p_origine la ville de l'origine.
+  //! \param[in]	p_destination la ville de destination.
+  //! \param[out]	p_duree le temps de vol total.
+  //! \return		Un vecteur contenant la liste des villes a parcourir.
+  //! \pre		Il y a assez de mémoire pour retourner les composantes du chemin.
+  //! \post		Le temps de vol total est retourné, -1 s'il n'y a pas de chemin.
+  //! \post		Le graphe original reste inchangé.
+  //! \exception	bad_alloc s'il n'y a pas assez de mémoire pour retourner le chemin.
+  //! \exception	logic_error si le sommet d'origine ou de destination n'existe pas.
+  std::vector<Ville> Couverture::determinerMinParcours(const Ville & p_origine, const Ville & p_destination, int & p_duree)
+  {
+    if (m_graphe.estVide())
+      throw std::logic_error("La couverture est vide.");
+    try
+      {
+	std::vector<Ville> chemin;
+	p_duree = m_graphe.dijkstra(p_origine, p_destination, chemin);
 	if (chemin.empty())
-		p_duree = -1;
+	  p_duree = -1;
 
-	return p_duree;
-}
+	return chemin;
+      }
+    catch(...)
+      {
+	throw;
+      }
+  }
 
-//! \brief		Trouve le plus court chemin entre deux points en utilisant l'algorithme de Bellman-Ford et le retourne.
-//! \param[in]	p_origine la ville de l'origine.
-//! \param[in]	p_destination la ville de destination.
-//! \param[out]	p_duree le temps de vol total.
-//! \return		Un vecteur contenant la liste des villes a parcourir.
-//! \pre		Il y a assez de mémoire pour retourner les composantes du chemin.
-//! \post		Le temps de vol total est retourné, -1 s'il n'y a pas de chemin.
-//! \post		Le graphe original reste inchangé.
-//! \exception	bad_alloc s'il n'y a pas assez de mémoire pour retourner le chemin.
-//! \exception	logic_error si le sommet d'origine ou de destination n'existe pas.
-std::vector<Ville> Couverture::bellManFord(const Ville & p_origine, const Ville & p_destination, int & p_duree)
-{
-	if (m_graphe == NULL || m_graphe.estVide())
-		throw std::logic_error("La couverture est vide.");
-	try
-	{
-		std::vector<Ville> chemin;
-		p_duree = m_graphe.bellmanFord(p_origine, p_destination, chemin);
-	}
-	catch(...)
-	{
-		throw;
-	}
+  //! \brief		Trouve le plus court chemin entre deux points en utilisant l'algorithme de Bellman-Ford et le retourne.
+  //! \param[in]	p_origine la ville de l'origine.
+  //! \param[in]	p_destination la ville de destination.
+  //! \param[out]	p_duree le temps de vol total.
+  //! \return		Un vecteur contenant la liste des villes a parcourir.
+  //! \pre		Il y a assez de mémoire pour retourner les composantes du chemin.
+  //! \post		Le temps de vol total est retourné, -1 s'il n'y a pas de chemin.
+  //! \post		Le graphe original reste inchangé.
+  //! \exception	bad_alloc s'il n'y a pas assez de mémoire pour retourner le chemin.
+  //! \exception	logic_error si le sommet d'origine ou de destination n'existe pas.
+  std::vector<Ville> Couverture::bellManFord(const Ville & p_origine, const Ville & p_destination, int & p_duree)
+  {
+    if (m_graphe.estVide())
+      throw std::logic_error("La couverture est vide.");
+    try
+      {
+	std::vector<Ville> chemin;
+	p_duree = m_graphe.bellmanFord(p_origine, p_destination, chemin);
 	if (chemin.empty())
-		p_duree = -1;
+	  p_duree = -1;
 
-	return p_duree;
-}
+	return chemin;
+      }
+    catch(...)
+      {
+	throw;
+      }
+  }
 }//namespace tp2
